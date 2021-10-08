97d210c3f66b3974347a120c66c79bf6a2efb88d
DeveloperYun <yhb7588@naver.com>
2021-10-09 01:34:51 +0900

    find, span

--- day2_3.py
+++ day2_3.py
@@ -1,1 +1,19 @@
-test
+import requests
+from bs4 import BeautifulSoup
+
+from homework3 import set_url
+
+indeed_result = requests.get("https://www.indeed.com/jobs?q=Python&limit=50")
+
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class":"pagination"})
+
+pages = pagination.find_all('a')
+spans = []
+for page in pages:
+    spans.append(page.find("span"))
+
+# print(spans[0:4])
+
+spans = spans[:-1]
