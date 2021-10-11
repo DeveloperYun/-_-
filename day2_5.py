2c49395f144b90e221be1c298f90e79b5773cc73
DeveloperYun <yhb7588@naver.com>
2021-10-11 12:38:03 +0900

    .

--- day2_5.py
+++ day2_5.py
@@ -1,6 +1,6 @@
 from indeed import get_jobs as get_indeed_jobs
 from stack_over_flow import get_jobs as get_so_jobs
-
+from save import save_to_file
 # #range - 입력한 수 만큼의 크기의 배열을 생성
 # for n in range(max_page):
 #     print(f"start={n*50}")
@@ -11,3 +11,7 @@ from stack_over_flow import get_jobs as get_so_jobs
 
 indeed_jobs = get_indeed_jobs()
 so_jobs = get_so_jobs()
+jobs = so_jobs+indeed_jobs
+save_to_file(jobs)
+
+#csv : comma separated value
